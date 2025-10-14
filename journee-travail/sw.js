98646390437c4f60fa98f3d59204c56c4ba36ea4
doudou120vf <vmangenet@gmaiL.com>
2025-10-14 15:54:41 +0200

    Add files via upload

---
+++ journee-travail/sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('travail-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './sw.js',
+        './style.css',
+        './script.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
